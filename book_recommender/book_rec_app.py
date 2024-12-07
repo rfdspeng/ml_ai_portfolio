9fb9f875ecc7470360cf3a5e3c3fc10db03fd24f
ryanwtsai <tsai.ryanw@gmail.com>
2024-12-07 15:48:33 -0800

    book_recommender/book_rec_app.py is working in non-streaming mode

--- book_recommender/book_rec_app.py
+++ book_recommender/book_rec_app.py
@@ -4,24 +4,9 @@ from agents import BookRetriever, BookAssistant
 # import time
 # import random
 
-# def response_generator():
-#     response = random.choice(
-#         [
-#             "Hello there! How can I assist you today?",
-#             "Hi, human! Is there anything I can help you with?",
-#             "Do you need help?",
-#         ]
-#     )
-
-#     for word in response.split():
-#         yield word + " "
-#         time.sleep(0.05)
-
 st.title("Welcome to the bookstore!")
 
-# Initialize chat history
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
+stream = False
 
 # Initialize session_state
 if "retriever" not in st.session_state:
@@ -29,11 +14,6 @@ if "retriever" not in st.session_state:
     st.session_state["retriever"] = BookRetriever(st.secrets.credentials, collection_name)
     st.session_state["assistant"] = BookAssistant(st.secrets.credentials["OPENAI_API_KEY"])
     st.session_state.messages = [] # I don't want to use assistant.messages_list since it exposes the search results from the retriever
-
-# Display chat messages from history on app rerun
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
 
 retriever = st.session_state["retriever"]
 assistant = st.session_state["assistant"]
@@ -46,10 +26,15 @@ for message in st.session_state.messages:
 
 # First time in the bookstore
 if st.session_state.messages == []:
-    welcome = assistant.respond(stream=False)
+    welcome = assistant.respond(stream=stream)
     with st.chat_message("assistant"):
-        st.markdown(welcome)
-    st.session_state.messages.append({"role": "assistant", "content": welcome})
+        if not stream:
+            st.markdown(welcome)
+            response = welcome
+        else:
+            response = st.write_stream(welcome)
+            # st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 # This expression assigns the user's input to prompt and checks that it's not None
 if prompt := st.chat_input("Your turn"):
@@ -61,20 +46,10 @@ if prompt := st.chat_input("Your turn"):
     # response = assistant.respond(prompt, search_results)
     # with st.chat_message("assistant"):
     #     st.markdown(response)
-    stream = assistant.respond(prompt, search_results, stream=True)
+    response = assistant.respond(prompt, search_results, stream=stream)
     with st.chat_message("assistant"):
-        response = st.write_stream(stream)
+        if not stream:
+            st.markdown(response)
+        else:
+            response = st.write_stream(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
-
-    # with st.chat_message("user"):
-    #     st.markdown(prompt)
-    # st.session_state.messages.append({"role": "user", "content": prompt})
-
-    # response = f"Echo: {prompt}"
-    # with st.chat_message("assistant"):
-    #     st.markdown(response)
-    # st.session_state.messages.append({"role": "assistant", "content": response})
-
-    # with st.chat_message("assistant"):
-    #     response = st.write_stream(response_generator())
-    # st.session_state.messages.append({"role": "assistant", "content": response})

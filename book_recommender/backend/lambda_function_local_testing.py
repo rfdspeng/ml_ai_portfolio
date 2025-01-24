1765567696fc4aa78842515b64ad5c123a96e682
ryanwtsai <tsai.ryanw@gmail.com>
2025-01-24 10:38:26 -0800

    book_recommender: completed Lambda with API Gateway integration (synchronous invocation)

--- book_recommender/backend/lambda_function_local_testing.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-from lambda_function import lambda_handler as async_lambda_handler
-
-def lambda_handler(event, context):
-    return asyncio.run(async_lambda_handler(event, context))

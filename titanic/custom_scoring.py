8ee624071d2246dfcc5068ce1452a7e8a7e53ca0
ryanwtsai <tsai.ryanw@gmail.com>
2025-07-18 13:13:11 -0700

    Titanic: add sample weights to grid search

--- titanic/custom_scoring.py
+++ titanic/custom_scoring.py
@@ -1,10 +1,10 @@
-from sklearn.metrics import make_scorer, accuracy_score, recall_score, precision_score, f1_score
+from sklearn.metrics import make_scorer, accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
 from typing import Literal
 import numpy as np
 
 # Per-category scorer
 def category_scorer(y_true, y_pred, categories, category,
-                 metric_type: Literal["accuracy", "recall", "precision", "f1"]):
+                 metric_type: Literal["accuracy", "recall", "precision", "f1", "tn", "tp", "fn", "fp"]):
     
     mask = categories == category # e.g. df["Title"] == "Mr"
     if metric_type == "accuracy":
@@ -15,10 +15,20 @@ def category_scorer(y_true, y_pred, categories, category,
         return precision_score(y_true[mask], y_pred[mask], zero_division=np.nan)
     if metric_type == "f1":
         return f1_score(y_true[mask], y_pred[mask], zero_division=np.nan)
+    
+    cm = confusion_matrix(y_true[mask], y_pred[mask], labels=[0,1])
+    if metric_type == "tn":
+        return cm[0,0]
+    if metric_type == "tp":
+        return cm[1,1]
+    if metric_type == "fn":
+        return cm[1,0]
+    if metric_type == "fp":
+        return cm[0,1]
 
 # Wrapper for make_scorer — it needs only (estimator, X, y)
 def make_scorer_wrapper(categories, category, 
-                        metric_type: Literal["accuracy", "recall", "precision", "f1"]):
+                        metric_type: Literal["accuracy", "recall", "precision", "f1", "tn", "tp", "fn", "fp"]):
     
     def scorer(estimator, X, y):
         y_pred = estimator.predict(X)
--- titanic/utility_functions.py
+++ titanic/utility_functions.py
@@ -2,11 +2,9 @@ from sklearn.model_selection import StratifiedKFold
 from pandas.core.frame import DataFrame
 from pandas.core.series import Series
 
-def make_stratified_k_fold_with_custom_strata(X: DataFrame, columns: list[str], kwargs={}):
+def make_custom_strata(X: DataFrame, columns: list[str]):
     if not columns:
-        raise Exception("make_stratified_k_fold_with_custom_strata: columns cannot be empty")
-    
-    cv_splitter = StratifiedKFold(**kwargs) # n_splits, shuffle, random_state
+        raise Exception("make_custom_strata: columns cannot be empty")
 
     y = X[columns].astype("string")
 
@@ -14,5 +12,16 @@ def make_stratified_k_fold_with_custom_strata(X: DataFrame, columns: list[str], 
 
     for col in columns[1:]:
         y_concat = y_concat.str.cat(y[col], sep="_", join="left")
+    
+    y_concat.name = "_".join(columns)
+    return y_concat
+
+def make_stratified_k_fold_with_custom_strata(X: DataFrame, columns: list[str], cv_kwargs={}):
+    if not columns:
+        raise Exception("make_stratified_k_fold_with_custom_strata: columns cannot be empty")
+    
+    cv_splitter = StratifiedKFold(**cv_kwargs) # n_splits, shuffle, random_state
+
+    y_concat = make_custom_strata(X, columns)
     
     return cv_splitter.split(X, y_concat) # Generator object. Iterating yields (train_indices, val_indices) (a 2-ple of numpy arrays)
